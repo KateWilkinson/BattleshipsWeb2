a68465123012a4ac2e75d77f6b7d24661a0c25bd
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-24 11:36:45 +0100

    Adjusted some wording and line breaks within the erb files

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -2,26 +2,29 @@ require 'spec_helper'
 
 feature 'I want to be able to play the game' do
 
-  scenario 'I want to be able to fire at a coordinate' do
-    visit '/new_game'
-    fill_in('name', with: 'Michael')
-    click_on 'Submit'
-    click_link 'Begin your adventure'
+  scenario 'I want to be able to fire at a ship' do
+    visit '/play_game'
+    expect(page).to have_content 'Enter the coordinate you want to shoot at'
+  end
+
+  scenario 'I want to be told if I\'ve hit a ship' do
+    visit '/setup_game'
     fill_in('coord', with: 'A1')
+    click_on 'Fire!'
     expect(page).to have_content 'hit'
   end
 
-  # scenario 'I want to be able to return to play_game and fire at another coordinate' do
-  #   visit '/hit_or_miss'
-  #   click_link 'Fire again'
-  #   expect(page).to have_content 'Enter the coordinate you want to hit!'
-  # end
+  scenario 'I want to be told if I\'ve missed a ship' do
+    visit '/setup_game'
+    fill_in('coord', with: 'B1')
+    click_on 'Fire!'
+    expect(page).to have_content 'miss'
+  end
 
-  # scenario 'The page will return miss! if I have missed a target'
-  #   visit '/play_game'
-  #   fill_in('coord', with: 'B4')
-  #   click_on 'Submit'
-  #   expect(page).to have_content 'Miss!'
-  # end
+  scenario 'I want to be able to keep firing at coordinates once I hit or miss' do
+    visit '/play_game'
+    fill_in('coord', with: 'A1')
+    expect(page).to have_content 'Enter the coordinate you want to shoot at'
+  end
 
 end
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 feature 'Starting a new game' do
   scenario 'I am asked to enter my name' do
     visit '/'
-    click_link 'Click to begin'
-    expect(page).to have_content "What is your name you seafaring scoundrel?"
+    click_link 'Register to play'
+    expect(page).to have_content "What's your name, new player?"
   end
 
   scenario 'I can enter my name' do
     visit '/new_game'
-    fill_in('name', with: 'Michael')
+    fill_in('name', with: 'Kate')
     click_on 'Submit'
-    expect(page).to have_content 'You are now registered, Michael!'
+    expect(page).to have_content 'You are now registered, Kate!'
 
   end
 
   scenario 'I can click a link to start the game' do
     visit '/new_game'
-    fill_in('name', with: 'Michael')
+    fill_in('name', with: 'Kate')
     click_on 'Submit'
     click_link 'Begin your adventure'
-    expect(page).to have_content 'Enter the coordinate you want to hit!'
+    expect(page).to have_content 'Enter the coordinate you want to shoot at'
   end
 
 end
